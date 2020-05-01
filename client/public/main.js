45e234cd322f37e76d8dfddc3b6d01459110d99e
paullampanaro <paul.lampanaro@outlook.com>
2020-04-30 21:46:40 -0400

    Update main.js
    More proof of concept and testing.

--- client/public/main.js
+++ client/public/main.js
@@ -4,10 +4,12 @@ require("dotenv").config();
 const http = require("http");
 
 const api = process.env.API
-const steam_id = process.env.STEAM_ID
-const url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?format=json&key=" + api + "&include_appinfo=true&steamid=" + steam_id;
+const steam_id_a = process.env.STEAM_ID_A
+const steam_id_b = process.env.STEAM_ID_B
+const url_a = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?format=json&key=" + api + "&include_appinfo=true&steamid=" + steam_id_a;
+const url_b = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?format=json&key=" + api + "&include_appinfo=true&steamid=" + steam_id_b;
 
-http.get(url, res => {
+http.get(url_a, res => {
     res.setEncoding("utf8");
     let body = "";
     res.on("data", data => {
@@ -16,5 +18,23 @@ http.get(url, res => {
     res.on("end", () => {
         body = JSON.parse(body);
         console.log(body);
+    });
+});
+
+http.get(url_b, res => {
+    res.setEncoding("utf8");
+    let body = "";
+    res.on("data", data => {
+        body += data;
+    });
+    res.on("end", () => {
+        body = JSON.parse(body);
+        console.log(body);
+
+        var games = body.response.games;
+        
+        for(let i = 0; i < games.length; i++) {
+            console.log(games[i].appid);
+        }
     });
 });
