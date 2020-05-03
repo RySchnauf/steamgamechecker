95439d1a09c15e76028ad08363ade98a5005a7d6
paullampanaro <paul.lampanaro@outlook.com>
2020-05-03 14:32:11 -0400

    Implement Promises
    First attempt at implementing Promises so that each API call can be made concurrently.

--- client/public/main.js
+++ client/public/main.js
@@ -1,6 +1,7 @@
 "use strict"
 
 require("dotenv").config();
+require("promise");
 const http = require("http");
 
 const api = process.env.API
@@ -9,32 +10,57 @@ const steam_id_b = process.env.STEAM_ID_B
 const url_a = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?format=json&key=" + api + "&include_appinfo=true&steamid=" + steam_id_a;
 const url_b = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?format=json&key=" + api + "&include_appinfo=true&steamid=" + steam_id_b;
 
-http.get(url_a, res => {
-    res.setEncoding("utf8");
-    let body = "";
-    res.on("data", data => {
-        body += data;
-    });
-    res.on("end", () => {
-        body = JSON.parse(body);
-        console.log(body);
-    });
-});
+let games_a = [];
+let games_b = [];
 
-http.get(url_b, res => {
-    res.setEncoding("utf8");
-    let body = "";
-    res.on("data", data => {
-        body += data;
-    });
-    res.on("end", () => {
-        body = JSON.parse(body);
-        console.log(body);
+// http.get(url_a, res => {
+//     res.setEncoding("utf8");
+//     let body = "";
+//     res.on("data", data => {
+//         body += data;
+//     });
+//     res.on("end", () => {
+//         body = JSON.parse(body);
+//         games_a = body.response.games;
+//         // console.log(body);
+//     });
+// });
 
-        var games = body.response.games;
-        
-        for(let i = 0; i < games.length; i++) {
-            console.log(games[i].appid);
-        }
+// based on "https://www.tomas-dvorak.cz/posts/nodejs-request-without-dependencies/"
+const getContent = function(url, game_array) {
+    return new Promise((resolve, reject) => {
+        const lib = require("http");
+        const request = lib.get(url, (response) => {
+            if(response.statusCode < 200 || response.statusCode > 299) {
+                reject(new Error('Failed to load page, status code: ' + response.statusCode));
+            }
+
+            response.setEncoding("utf8");
+            let body = "";
+            response.on('data', data => {
+                body += data;
+            });
+            response.on("end", () => {
+                body = JSON.parse(body);
+                game_array = body.response.games;
+                console.log(body);
+            });
+        });
+        request.on("error", (err) => reject(err))
+    })
+};
+
+const promise_a = getContent(url_a, games_a)
+    .then((html) => console.log(html))
+    .catch((err) => console.error(err));
+
+const promise_b = getContent(url_b, games_b)
+    .then((html) => console.log(html))
+    .catch((err) => console.log(err));
+
+// not sure this is the correct syntax
+Promise.all([promise_a, promise_b])
+    .then(() => {
+        console.log(games_a);
+        console.log(games_b);
     });
-});
