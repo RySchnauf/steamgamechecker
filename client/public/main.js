3b90d19360aaa1eefc5b30b1b3d829ea6bcb6b94
paullampanaro <paul.lampanaro@outlook.com>
2020-05-09 20:21:20 -0400

    Variable number of steam ID's
    Now accomodates a variable number of steam ID's.

--- client/public/main.js
+++ client/public/main.js
@@ -7,14 +7,12 @@ const http = require("http");
 const api = process.env.API
 const steam_id_a = process.env.STEAM_ID_A
 const steam_id_b = process.env.STEAM_ID_B
-const url_a = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?format=json&key=" + api + "&include_appinfo=true&steamid=" + steam_id_a;
-const url_b = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?format=json&key=" + api + "&include_appinfo=true&steamid=" + steam_id_b;
 
-let games_a = [];
-let games_b = [];
+var dict = {};
+var steam_ids = [steam_id_a, steam_id_b];
 
 // based on "https://www.tomas-dvorak.cz/posts/nodejs-request-without-dependencies/"
-const getContent = function(url, game_array) {
+const getContent = function(url) {
     return new Promise((resolve, reject) => {
         const lib = require("http");
         const request = lib.get(url, (response) => {
@@ -29,7 +27,7 @@ const getContent = function(url, game_array) {
             });
             response.on("end", () => {
                 body = JSON.parse(body);
-                game_array = body.response.games;
+                var game_array = body.response.games;
                 // console.log(body);
                 resolve(game_array);
             });
@@ -38,29 +36,47 @@ const getContent = function(url, game_array) {
     });
 };
 
-const promise_a = getContent(url_a, games_a)
-    // .then((message) => console.log(message))
-    // .catch((err) => console.error(err));
+// const promise_a = getContent(url_a, games_a)
+//     // .then((message) => console.log(message))
+//     // .catch((err) => console.error(err));
 
-const promise_b = getContent(url_b, games_b)
-    // .then((message) => console.log(message))
-    // .catch((err) => console.log(err));
-
-Promise.all([promise_a, promise_b])
-    .then((values) => {
-        let dict = {};
-
-        // loop through each game library, add game as key and users as value
-        for(var i = 0; i < values.length; i++) {
-            values[i].forEach(element => {
+const collectLibrary = function(url, steam_id) {
+    return new getContent(url)
+        .then((message) => {
+            message.forEach(element => {
                 if(dict[element.appid]) {
-                    dict[element.appid].push(i);
+                    dict[element.appid].push(steam_id);
                 }
                 else {
-                    dict[element.appid] = [i];
+                    dict[element.appid] = [steam_id];
                 }
             });
-        }
+        })
+        .catch((err) => console.log(err));
+};
+
+var promises = [];
+
+steam_ids.forEach(element => {
+    let url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?format=json&key=" + api + "&include_appinfo=true&steamid=" + element;
+    promises.push(collectLibrary(url, element));
+});
+
+Promise.all(promises)
+    .then((values) => {
+        // let dict = {};
+
+        // loop through each game library, add game as key and users as value
+        // for(var i = 0; i < values.length; i++) {
+        //     values[i].forEach(element => {
+        //         if(dict[element.appid]) {
+        //             dict[element.appid].push(i);
+        //         }
+        //         else {
+        //             dict[element.appid] = [i];
+        //         }
+        //     });
+        // }
 
         // print out
         for(var key in dict) {
